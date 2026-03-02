fdab23ef7ca1e3c6a1a1bacc19a02724d962dcc4
Delta Block <deltablockv@gmail.com>
2026-03-02 08:15:15 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import ProblemSection from "../components/ProblemSection";
 import ServicesSection from "../components/ServicesSection";
 import HowItWorksSection from "../components/HowItWorksSection";
-import BeforeAfterSection from "../components/BeforeAfterSection";
+import WhySolarSection from "../components/WhySolarSection";
+import SavingsCalculator from "../components/SavingsCalculator";
+import PanelCleaningSection from "../components/PanelCleaningSection";
+import InstallationGallery from "../components/InstallationGallery";
+import CaliforniaNewsSection from "../components/CaliforniaNewsSection";
+import CaliforniaProgramsSection from "../components/CaliforniaProgramsSection";
 import LeadFormSection from "../components/LeadFormSection";
+import FAQSection from "../components/FAQSection";
 import CtaSection from "../components/CtaSection";
 import FooterSection from "../components/FooterSection";
 import StickyCallBar from "../components/StickyCallBar";
+import DarkModeToggle from "../components/DarkModeToggle";
 
 export default function Home() {
+  const [dark, setDark] = useState(false);
+
   return (
-    <div className="font-sans">
-      <HeroSection />
-      <ProblemSection />
-      <ServicesSection />
-      <HowItWorksSection />
-      <BeforeAfterSection />
-      <LeadFormSection />
-      <CtaSection />
-      <FooterSection />
-      <StickyCallBar />
+    <div className={dark ? "dark" : ""}>
+      <div className="font-sans bg-white dark:bg-gray-900 transition-colors duration-300">
+        <DarkModeToggle dark={dark} onToggle={() => setDark(!dark)} />
+        <HeroSection />
+        <ProblemSection />
+        <WhySolarSection />
+        <ServicesSection />
+        <HowItWorksSection />
+        <SavingsCalculator />
+        <PanelCleaningSection />
+        <InstallationGallery />
+        <CaliforniaNewsSection />
+        <CaliforniaProgramsSection />
+        <LeadFormSection />
+        <FAQSection />
+        <CtaSection />
+        <FooterSection />
+        <StickyCallBar />
+      </div>
     </div>
   );
 }
